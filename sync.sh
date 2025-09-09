ef9e58cd8e7a82ca03f785176f8fa8ffcc9761c7
Didier Verna <didier@didierverna.net>
2025-09-09 19:02:30 +0200

    Hack resume-experience.

--- sync.sh
+++ sync.sh
@@ -6,6 +6,7 @@ dest=../didierverna.net
 
 cp $src/blox/resume-biography-3/block.html $dest/layouts/partials/hbx/blocks/resume-biography-3/
 cp $src/blox/resume-awards/block.html $dest/layouts/partials/hbx/blocks/resume-awards/
+cp $src/blox/resume-experience/block.html $dest/layouts/partials/hbx/blocks/resume-experience/
 
 cp $src/layouts/single.html $dest/layouts/
 cp $src/layouts/_partials/site_footer.html $dest/layouts/partials/
