0230a325f8d71c2842705824330908b8686ff261
Didier Verna <didier@didierverna.net>
2025-09-09 18:49:57 +0200

    Fix sync script.

--- sync.sh
+++ sync.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Synchronize my Hugo Blox overrides with the net site.
-$src=modules/blox-tailwind
-$dest=../didierverna.net/layouts
+src=modules/blox-tailwind
+dest=../didierverna.net
 
 cp $src/blox/resume-biography-3/block.html $dest/layouts/partials/hbx/blocks/resume-biography-3/
 
