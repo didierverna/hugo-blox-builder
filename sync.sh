049136dff07d4832f3937d2e0390c9cf8c72ec3f
Didier Verna <didier@didierverna.net>
2025-09-09 18:56:39 +0200

    Hack resume-awards.

--- sync.sh
+++ sync.sh
@@ -5,6 +5,7 @@ src=modules/blox-tailwind
 dest=../didierverna.net
 
 cp $src/blox/resume-biography-3/block.html $dest/layouts/partials/hbx/blocks/resume-biography-3/
+cp $src/blox/resume-awards/block.html $dest/layouts/partials/hbx/blocks/resume-awards/
 
 cp $src/layouts/single.html $dest/layouts/
 cp $src/layouts/_partials/site_footer.html $dest/layouts/partials/
